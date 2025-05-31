bcbf0744cfbfb3596f50171fcfb94263cf5c35a1
jjjjuuuudyy <140327729+jjjjuuuudyy@users.noreply.github.com>
2025-05-31 23:08:26 +0800

    revise

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from linebot.models import *
 #======python的函數庫==========
 import tempfile, os
 import datetime
-from openai import OpenAI
+import openai
 import time
 import traceback
 #======python的函數庫==========
@@ -23,16 +23,14 @@ line_bot_api = LineBotApi(os.getenv('CHANNEL_ACCESS_TOKEN'))
 # Channel Secret
 handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
 # OPENAI API Key初始化設定
-client = OpenAI(
-    api_key = os.getenv("GITHUB_TOKEN"),
-    base_url="https://models.github.ai/inference"
-)
+openai.api_key = os.getenv("GITHUB_TOKEN")
+openai.api_base = "https://models.github.ai/inference"
 
 
 def GPT_response(text):
     # 接收回應
     # response = openai.Completion.create(model="gpt-3.5-turbo-instruct", prompt=text, temperature=0.5, max_tokens=500)
-    response = client.chat.completions.create(
+    response = openai.ChatCompletion.create(
         model="openai/gpt-4o",
         messages=[
             {"role": "system", "content": text}
@@ -41,7 +39,7 @@ def GPT_response(text):
     print(response)
     # 重組回應
     # answer = response['choices'][0]['text'].replace('。','')
-    answer = response.choices[0].message.content
+    answer = response["choices"][0]["message"]["content"]
     return answer
 
 
